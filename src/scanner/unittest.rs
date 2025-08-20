12cb913d1d8f364990c7016dea080f07c0655c99
timothy adigun <adigun.timothy@gmail.com>
2025-08-20 16:21:15 +0100

    modified scanner unittest function

--- src/scanner/unittest.rs
+++ src/scanner/unittest.rs
@@ -20,3 +20,15 @@ fn test_new_associate_function() {
         }
     );
 }
+
+#[test]
+fn test_next_bytes_function() {
+    let mut scan = Scanner::from_str("Green latern");
+    scan.next(); // 0 - G
+    scan.next(); // 1 - r
+    scan.next(); // 3 - e
+    scan.next(); // 4 - e
+    scan.next(); // 5 - n
+    scan.next(); // 6 - ' '
+    assert_eq!(scan.next_byte(), Some(108)); // next?
+}
