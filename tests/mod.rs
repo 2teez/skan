1362c7e26f0e6c55d9285fdb64faa06d601f09a1
timothy adigun <adigun.timothy@gmail.com>
2025-08-20 15:04:20 +0100

    added test for has_next to check limit

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,1 +1,19 @@
+use skan::scanner::Scanner;
 
+#[test]
+pub fn test_string_with_zero_length() {
+    let scan = Scanner::from_str("");
+    assert_eq!(scan.has_next(), false);
+}
+
+#[test]
+pub fn test_string_with_single_length() {
+    let scan = Scanner::from_str("1");
+    assert_eq!(scan.has_next(), true);
+}
+
+#[test]
+pub fn test_next_implmentation_function() {
+    let mut scan = Scanner::from_str("java");
+    assert_eq!(scan.next(), Some("java".as_bytes()[0]));
+}
