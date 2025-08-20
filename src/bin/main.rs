2f7bd496dcebcfce46bbb94d746b0aca88b83bd2
timothy adigun <adigun.timothy@gmail.com>
2025-08-20 21:15:16 +0100

    save the main.rs file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,21 @@
 #![allow(dead_code, unused)]
 
-fn main() {}
+use skan::scanner::Scanner;
+use std::fs::{self, File};
+use std::io::{self, Read, Write};
+
+fn main() {
+    let mut scan = Scanner::from_str("23 78 9");
+    scan.next_word();
+    println!("{:?}", scan.next_word());
+    /*for value in scan {
+        print!("{:}", value as char);
+    }
+
+    print!("\nEnter words: (Ctrl+D to Exit): ");
+    io::stdout().flush().unwrap();
+    let scan = Scanner::new(io::stdin());
+    for value in scan {
+        print!("{:}", value as char);
+    }*/
+}
